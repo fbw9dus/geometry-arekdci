800b5055dd211f39b9a4727ab204732cc9c9d04a
arekdci <arekciura85@gmail.com>
2019-11-19 16:18:34 +0100

    x

--- shapes.js
+++ shapes.js
@@ -10,6 +10,63 @@ var rectangle = {
 
 var parallelogram = {
   width: 70,
-  height: 40
+  height: 40,
   color: "red"
 }
+
+var one = document.querySelector(".board")
+var circle1 = document.createElement("div")
+var rectangle1 = document.createElement("div")
+var rectangleparallelogram1 = document.createElement("div")
+var spaceinvider1 = document.createElement("div")
+
+
+one.appendChild(circle1)
+one.appendChild(rectangle1)
+one.appendChild(rectangleparallelogram1)
+one.appendChild(spaceinvider1)
+
+circle1.classList.add("circle")
+rectangle1.classList.add("rectangle")
+rectangleparallelogram1.classList.add("parallelogram")
+spaceinvider1.classList.add("space-invader")
+
+
+circle1.style.width = circle.size + "px"
+circle1.style.height = circle.size + "px"
+circle1.style.backgroundColor = circle.color
+
+
+rectangle1.style.width = rectangle.size + "px"
+rectangle1.style.height = rectangle.size + "px"
+rectangle1.style.backgroundColor = rectangle.color
+
+rectangleparallelogram1.style.width = parallelogram.width + "px"
+rectangleparallelogram1.style.height = parallelogram.height + "px"
+rectangleparallelogram1.style.backgroundColor = parallelogram.color
+
+spaceinvider1.style.width = space - invider.width + "em"
+spaceinvider1.style.hight = space - invider.height + "em"
+spaceinvider1.style.backgroundColor = space - invider
+spaceinvider1.style.boxShadow = space - invider + "em"
+spaceinvider1.style.overflow = space - invider
+spaceinvider1.style.margin = space - invider
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
